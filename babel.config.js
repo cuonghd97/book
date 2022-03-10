de49fc2edfec12c3402f17e0cd93f0304a24d896
DESKTOP-E8OTJ33\Cuong <cuong.hoangduc@cxview.ai>
2022-03-10 16:26:52 +0000

    Update config

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      require.resolve('babel-plugin-module-resolver'),
-      {
-        alias: {
-          src: './src',
-          '@components': './src/components',
-          '@locales': './src/locales',
-          '@routes': './src/routes',
-          '@navigator': './src/navigator',
-          '@styles': './src/styles',
-          '@store': './src/store',
-          '@utils': './src/utils',
-          '@models': './src/models',
-          '@lib': './src/lib',
-          '@assets': './src/assets',
-          '@errors': './src/errors',
-        },
-      },
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['.'],
+                alias: {
+                    src: './src',
+                    '@components': './src/components',
+                    '@locales': './src/locales',
+                    '@routes': './src/routes',
+                    '@navigator': './src/navigator',
+                    '@styles': './src/styles',
+                    '@store': './src/store',
+                    '@utils': './src/utils',
+                    '@models': './src/models',
+                    '@lib': './src/lib',
+                    '@assets': './src/assets',
+                    '@errors': './src/errors',
+                },
+            },
+        ],
     ],
-  ],
 };
